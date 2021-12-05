56fd3f34e61f1109879b54913fca2c628f2173ed
Aleksandra <a.haniok@gmail.com>
2021-12-05 11:34:13 +0000

    Add code to increment score and incorrect answers tally

--- assets/js/script.js
+++ assets/js/script.js
@@ -16,7 +16,7 @@ document.addEventListener("DOMContentLoaded", function(){
     }
     runGame("addition");
 
-})
+});
 
 /**
  * The main game "loop", called when the script is first loaded
@@ -24,33 +24,41 @@ document.addEventListener("DOMContentLoaded", function(){
  * 
  */
 
-function runGame(gameType){
+ function runGame(gameType) {
+
     // Creates two random numbers between 1 and 25
     let num1 = Math.floor(Math.random() * 25) + 1;
     let num2 = Math.floor(Math.random() * 25) + 1;
 
-    if (gameType === "addition"){
+    if (gameType === "addition") {
         displayAdditionQuestion(num1, num2);
     } else {
         alert(`Unknown game type: ${gameType}`);
         throw `Unknown game type: ${gameType}. Aborting!`;
     }
+
 }
+
 /**
  * Checks the answer against the first element in
  * the returned calculateCorrectAnswer array
  */
-function checkAnswer(){
-    let userAnswer = parseInt(document.getElementById('answer-box').value);
+ function checkAnswer() {
+
+    let userAnswer = parseInt(document.getElementById("answer-box").value);
     let calculatedAnswer = calculateCorrectAnswer();
     let isCorrect = userAnswer === calculatedAnswer[0];
 
-    if (isCorrect){
-        alert("Hey! You got it right!");
+    if (isCorrect) {
+        alert("Hey! You got it right! :-)");
+        incrementScore();
     } else {
-        alert (`Awww... you answered ${userAnswer}. The correct answer was ${calculatedAnswer[0]}!`);
+        alert(`Oh... you answered ${userAnswer}. The correct answer was ${calculatedAnswer[0]}!`);
+        incrementWrongAnswer();
     }
+
     runGame(calculatedAnswer[1]);
+
 }
 
 /**
@@ -58,11 +66,13 @@ function checkAnswer(){
  * directly from the dom, and returns the correct answer.
  */
 
-function calculateCorrectAnswer(){
     // ParseInt is used to convert the text taken from the DOM to a number
+
+function calculateCorrectAnswer() {
+
     let operand1 = parseInt(document.getElementById('operand1').innerText);
     let operand2 = parseInt(document.getElementById('operand2').innerText);
-    let operator = document.getElementById('operator').innerText;
+    let operator = document.getElementById("operator").innerText;
 
     if (operator === "+") {
         return [operand1 + operand2, "addition"];
@@ -70,14 +80,23 @@ function calculateCorrectAnswer(){
         alert(`Unimplemented operator ${operator}`);
         throw `Unimplemented operator ${operator}. Aborting!`;
     }
-}
-
-function incrementScore(){
 
 }
 
+/**
+ * Gets the current score from the DOM and increments it by 1
+*/
+function incrementScore() {
+    let oldScore = parseInt(document.getElementById('score').innerText);
+    document.getElementById('score').innerText = ++oldScore;
+}
+
+/**
+ * Gets the current tally of incorrect answers from the DOM and increments it by 1
+*/
 function incrementWrongAnswer(){
-
+let oldScore = parseInt(document.getElementById('incorrect').innerText);
+    document.getElementById('incorrect').innerText = ++oldScore;
 }
 
 function displayAdditionQuestion(operand1, operand2) {
